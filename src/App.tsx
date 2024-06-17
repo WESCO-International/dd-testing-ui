2aa768f94ed06cbeae6b94a45e4ad2797da5d6db
RamanareddyPalugulla <Ramanareddy.Palugulla@wescodist.com>
2024-06-17 17:03:13 +0530

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import logo from './logo.svg'
 import ThemeProvider from './shared/ThemeProvider/ThemeProvider'
-import { AppHeader, Link, Image, Text } from './App.styles'
+import { AppHeader, Image } from './App.styles'
 import { Home } from './Components/Home'
 
 const App: React.FunctionComponent = () => {
@@ -10,18 +10,6 @@ const App: React.FunctionComponent = () => {
         <Image src={logo} alt="logo" />
       </AppHeader>
       <Home />
-      <Text padding={'10px'} fontSize={4}>
-        For ref #
-      </Text>
-      <Link data-testid="learn-react" padding="5px" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-        Learn React,
-      </Link>
-      <Link data-testid="learn-goober" padding="5px" href="https://goober.js.org" target="_blank" rel="noopener noreferrer">
-        Learn Goober,
-      </Link>
-      <Link data-testid="learn-styled-system" padding="5px" href="https://styled-system.com" target="_blank" rel="noopener noreferrer">
-        Learn Styled System
-      </Link>
     </ThemeProvider>
   )
 }
--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,23 +1,55 @@
 import { Container } from './Home.styles'
-
 import { Text } from '../../App.styles'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import { Button } from 'react-bootstrap'
 
 const Home: React.FunctionComponent = () => {
+  const [name, setName] = useState('')
+  const [data, setData] = useState([])
+  const [response, setResponse] = useState([])
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:8080/testing')
+      .then(response => {
+        setData(response.data)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }, [])
+
+  const callYourAPI = () => {
+    if (name) {
+      axios
+        .get(`http://localhost:8080/testing/${name}`)
+        .then(res => {
+          setResponse(res.data)
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    }
+  }
+
   return (
     <Container data-testid="home-container" padding={'10px'}>
       <Text fontSize={10} fontWeight={6} padding={'10px'}>
-        Sample on Styled system with goober
+        <div>{data ? <pre>{data}</pre> : 'Loading...'}</div>{' '}
       </Text>
-      <Text fontSize={2} padding={'10px'}>
-        By Passing value to fontSize props as 2 will use the theme font size array index of 2
-      </Text>
-      <Text fontSize={5} padding={'10px'}>
-        By Passing value to fontSize props as 5 will use the theme font size array index of 5
-      </Text>
-      <Text fontSize={[1, 2, 8]} padding={'10px'}>
-        Similary passing an array to the styled props will generate responsive design, check this on responsive mode. Same goes for each and every
-        component which is wrapped with styled-system
-      </Text>
+      <form>
+        <label>
+          Enter your value:
+          <input type="text" value={name} onChange={e => setName(e.target.value)} />
+        </label>
+        <Button type="submit" variant="success" onClick={() => callYourAPI()}>
+          Submit
+        </Button>
+      </form>
+      <div>
+        <div>{response ? <pre>{response}</pre> : 'Loading...'}</div>{' '}
+      </div>
     </Container>
   )
 }
