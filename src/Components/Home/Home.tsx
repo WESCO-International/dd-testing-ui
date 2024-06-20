40ac81a82b80d05bb3200cef60672a36cc7abc88
RamanareddyPalugulla <Ramanareddy.Palugulla@wescodist.com>
2024-06-20 15:22:23 +0530

    user api integrated

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,13 +1,13 @@
 import { Container } from './Home.styles'
-import { Text } from '../../App.styles'
 import { useEffect, useState } from 'react'
 import axios from 'axios'
-import { Button } from 'react-bootstrap'
+import { Button, Card, Col, Form, ListGroup, Row } from 'react-bootstrap'
 
 const Home: React.FunctionComponent = () => {
-  const [name, setName] = useState('')
   const [data, setData] = useState([])
-  const [response, setResponse] = useState([])
+  const [users, setUsers] = useState([])
+  const [msg, setMsg] = useState('')
+  const [validated, setValidated] = useState<boolean>(false)
 
   useEffect(() => {
     axios
@@ -20,41 +20,94 @@ const Home: React.FunctionComponent = () => {
       })
   }, [])
 
-  const callYourAPI = () => {
-    if (name) {
-      axios
-        .get(`http://localhost:8080/testing/${name}`)
-        .then(res => {
-          setResponse(res.data)
-        })
-        .catch(error => {
-          console.log(error)
-        })
+  useEffect(() => {
+    axios
+      .get('http://localhost:8080/user')
+      .then(response => {
+        setUsers(response.data)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }, [])
+
+  const handleSubmit = (event: any) => {
+    event.preventDefault()
+    const form = event.currentTarget
+    if (form.checkValidity() === false) {
+      event.stopPropagation()
+      setValidated(true)
+      return
     }
+    setValidated(false)
+
+    const formData = new FormData(event.target)
+    axios
+      .post(`http://localhost:8080/user`, {
+        userName: formData.get('username'),
+        password: formData.get('password'),
+      })
+      .then(res => {
+        setMsg('User ' + res.data.userName + ' created successfully')
+
+        axios
+          .get('http://localhost:8080/user')
+          .then(response => {
+            setUsers(response.data)
+          })
+          .catch(error => {
+            console.log(error)
+          })
+      })
   }
 
-  return (
+  http: return (
     <Container data-testid="home-container" padding={'10px'}>
-      <Text fontSize={10} fontWeight={6} padding={'10px'}>
-        <div>{data ? <pre>{data}</pre> : 'Loading...'}</div>{' '}
-      </Text>
-      <form>
-        <label>
-          Enter your value:
-          <input type="text" value={name} onChange={e => setName(e.target.value)} />
-        </label>
-        <Button
-          type="submit"
-          variant="success"
-          onClick={e => {
-            e.preventDefault()
-            callYourAPI()
-          }}
-        >
-          Submit
-        </Button>
-      </form>
-      <div>{response}</div>
+      <Card>
+        <Card.Body>{data}</Card.Body>
+      </Card>
+      <br></br>
+      <Row>
+        <Col>
+          <Card>
+            <Card.Body>
+              <Card.Title>Create User</Card.Title>
+              <Form noValidate validated={validated} onSubmit={handleSubmit}>
+                <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                  <Form.Label>Username</Form.Label>
+                  <Form.Control required name="username" type="text" placeholder="username" />
+                  <Form.Control.Feedback type="invalid">Username is required</Form.Control.Feedback>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="exampleForm.ControlInput2">
+                  <Form.Label>password</Form.Label>
+                  <Form.Control required name="password" type="password" placeholder="password" />
+                  <Form.Control.Feedback type="invalid">Password is required </Form.Control.Feedback>
+                </Form.Group>
+                <div className="d-grid gap-2" style={{ textAlign: 'center' }}>
+                  <Button variant="success" type="submit">
+                    Submit
+                  </Button>
+                </div>
+              </Form>
+              <div className="mt-2 ml-1 text-success">{msg}</div>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col>
+          <Card>
+            <Card.Body>
+              <Card.Title>Users</Card.Title>
+              <ListGroup as="ol" numbered>
+                {users.map((user: any) => (
+                  <ListGroup.Item key={user.id} as="li">
+                    {user.id} | {user.userName} | {user.password}
+                  </ListGroup.Item>
+                ))}
+              </ListGroup>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
     </Container>
   )
 }
